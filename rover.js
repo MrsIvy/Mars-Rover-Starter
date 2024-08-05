4ae7f08b291574a0839947c0843a15c97beb9e0e
LaTanya Ivy <latanyaivy@gmail.com>
2024-08-04 19:36:26 -0500

    Debug

--- rover.js
+++ rover.js
@@ -4,7 +4,7 @@ class Rover {
       if (!position) {
          throw Error("Postion required.");
       }
-      this.mode = "Normal";
+      this.mode = "NORMAL";
       this.generatorWatts = 110;
    };
    receiveMessage(message) {
@@ -13,20 +13,39 @@ class Rover {
       // let roverStatus = response.results[1].roverStatus;
       // iterate over the commands array for loop message.commands.length
       // check the commandtype conditional if/else if/else if/ else(push {completed: true}
-      for (let i = 0; i > message.commands.length; i++) {
-         if (message.commands[i].commandtype === 'STATUS_CHECK') {
+      for (let i = 0; i < message.commands.length; i++) {
+         if (message.commands[i].commandType === 'STATUS_CHECK') {
             results.push({
                completed: true,
                roverStatus: {
                   mode: this.mode,
                   generatorWatts: 110,
                   position: 5860
-               } 
-           
-
+               }
             });
-            //  return { results: results};
+         } else if (message.commands[i].commandType === "MODE_CHANGE") {
+            this.mode = message.commands[i].value;
+            results.push({
+               completed: true
+            })
+         } else if (message.commands[i].commandType === "MOVE") {
+            if (this.mode === "LOW_POWER") {
+               results.push({
+                  completed: false
+               })
+            } else {
+               this.position = message.commands[i].value;
+               results.push({
+                  completed: true
+               })
+            }
+
+         } else {
+            results.push({
+               completed: true
+            })
          }
+
       }
       // if message.commands[i].commandtype is STATUS_CHECK: 
       // finally .push status objects into the results array
@@ -38,8 +57,7 @@ class Rover {
       // if message.commands[i].commandtype is MOVE:
       // check the mode and if MODE is LOW_POWER push in {completed: false}
       // else: udpate the position of the rover to the value (message.commands[i].value)
-      // Push to the reasults array[] results.push {completed: true}
-
+      // Push to the results array[] results.push {completed: true}
 
       return { message: message.name, results: results }
 
@@ -50,7 +68,7 @@ class Rover {
 // let response return an object containing 2 properties
 // message is the key value is message.name
 // results is a key empty array [] is value 
-;
+
 
 
 
--- spec/rover.spec.js
+++ spec/rover.spec.js
@@ -13,7 +13,7 @@ describe("Rover class", function () {
 
     let rover = new Rover(98382);
     expect(rover.position).toBe(98382);
-    expect(rover.mode).toBe("Normal");
+    expect(rover.mode).toBe("NORMAL");
     expect(rover.generatorWatts).toBe(110);
   });
 
@@ -24,17 +24,17 @@ describe("Rover class", function () {
     let rover = new Rover(5860);
     let response = rover.receiveMessage(message);
     expect(response.message).toBe("LaTanya");
-    console.log(response);
+
   });
 
   it("will give a response returned by receiveMessage includes two results if two commands are sent in the message", function () {
 
-    let commands = [new Command("MOVE", 99482), new Command("STATUS_CHECK")];
+    let commands = [new Command("STATUS_CHECK"), new Command("STATUS_CHECK")];
     let message = new Message("LaTanya", commands);
     let rover = new Rover(5860);
     let response = rover.receiveMessage(message);
     expect(response.results.length).toBe(commands.length);
-    console.log(response);
+
   });
 
   it("responds correctly to the status check command", function () {
@@ -51,10 +51,9 @@ describe("Rover class", function () {
         position: 5860
       }
     }
-    expect(response.results[0]).toBe("STATUS_CHECK");
 
+    expect(response.results[0]).toEqual(status);
 
-    console.log(response);
     // I know that STATUS_CHECK doesn't require a value
     // but I dont know why this test is passing 
     // and if I put something in the toBe it fails
@@ -62,48 +61,47 @@ describe("Rover class", function () {
 
   it("responds correctly to the mode change command", function () {
 
-    let commands = [new Command("MOVE", 99482), new Command("STATUS_CHECK")];
+    let commands = [new Command("MODE_CHANGE", "LOW_POWER"), new Command("STATUS_CHECK")];
     let message = new Message("LaTanya", commands);
     let rover = new Rover(5860);
     let response = rover.receiveMessage(message);
-   
-    expect(response.mode).toBe("LOW_POWER");
-    // expect(response.commands[1].result).toBe("LOW_POWER");
 
-    console.log(response);
+    expect(rover.mode).toBe("LOW_POWER");
 
   });
 
-  test.todo("responds with a false completed value when attempting to move in LOW_POWER mode"), function () {
+  it("responds with a false completed value when attempting to move in LOW_POWER mode", function () {
 
     let commands = [new Command("MOVE", 99482), new Command("STATUS_CHECK")];
     let message = new Message("LaTanya", commands);
     let rover = new Rover(5860);
-    let response = rover.receiveMessage(message);
+    let test = rover.receiveMessage(message);
     let firstPosition = rover.position;
-  }
-  expect(response.commands[0].completed).toBe(false);
-  expect(rover.position).toBe(firstPosition);
+
+    expect(test.results[0].completed).toBe(false);
+    expect(rover.position).toBe(firstPosition);
+  })
+console.log(test.results[0])
   // If feel like I should use commandtype and then mode right 
   // here to change mode from NORMAL to LOW_POWER 
 
-  console.log(response);
+
 })
 
-it("responds with the position for the move command"), function () {
+it("responds with the position for the move command", function () {
 
   let commands = [new Command("MOVE", 99482), new Command("STATUS_CHECK")];
   let message = new Message("LaTanya", commands);
   let rover = new Rover(5860);
   let response = rover.receiveMessage(message);
 
-  expect(response.commands[1].completed).toBe(true);
+  expect(response.commands.completed).toBe(true);
   expect(rover.position).toBe(99482);
 
   console.log(response);
-}
+});
 
-// If feel like I should use commandtype and then mode right 
+// If feel like I should use commandtype and then mode right
 // here to change mode from NORMAL to LOW_POWER 
 
 
